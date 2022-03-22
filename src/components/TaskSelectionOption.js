9e942f4415e8ba2a133c6a5a6bddbccead207d1b
zeka-mashi <lin.david32@gmail.com>
2022-03-21 20:52:50 -0700

    remove id from data file

--- src/components/TaskSelectionOption.js
+++ src/components/TaskSelectionOption.js
@@ -5,14 +5,14 @@ const TaskSelectionOption = (props) => {
         return <select id="showSelection" key="showSelection" name="Shown" size="10">
             {mapleData.map(data => {
                 if (!(localStorage.getItem('show ' + data.name) === "false")) {
-                    return <option key={data.id} value={data.name}>[{data.is}] {data.name}</option>
+                    return <option key={data.name} value={data.name}>[{data.is}] {data.name}</option>
                 }
             })}</select>
     } else {
         return <select id="hideSelection" key="hideSelection" name="Hidden" size="10">
             {mapleData.map(data => {
                 if (localStorage.getItem('show ' + data.name) === "false") {
-                    return <option key={data.id} value={data.name}>[{data.is}] {data.name}</option>
+                    return <option key={data.name} value={data.name}>[{data.is}] {data.name}</option>
                 }
             })}</select>
     }
